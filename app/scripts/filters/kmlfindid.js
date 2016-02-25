d7119df5aa53202af43c6dda2d6246bdef438632
Kamaal Aboothalib <kamaal@kamaal.me>
2016-02-25 23:47:34 +0400

    Done!
    Done adding genre

--- app/scripts/filters/kmlfindid.js
+++ app/scripts/filters/kmlfindid.js
@@ -10,7 +10,14 @@
  */
 angular.module('themoviedbApp')
   .filter('kmlFindId', function () {
-    return function (input) {
-      return 'kmlFindId filter: ' + input;
+    return function (input, id) {
+      var i,
+          len=input.length;
+      for (i=0; i<len; i++) {
+          if (+input[i].id === +id) {
+              return input[i];
+          }
+      }
+      return null;
     };
   });
